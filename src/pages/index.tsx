1de28a7e84a79fc280e7c288954a49fb4d83fe64
Keith <fungsenkeith@gmail.com>
2023-11-29 11:45:36 -0500

    working roulette logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,30 @@
 import Head from "next/head";
 import { useState, useEffect } from "react";
 
-import { api } from "~/utils/api";
-
 export default function Home() {
   // const hello = api.post.hello.useQuery({ text: "from tRPC" });
   const [currentNum, setCurrentNum] = useState<number>(1000);
   const [rouletteNum, setRouletteNum] = useState<number | null>(null);
   const [status, setStatus] = useState<null | string>(null);
 
-  const rollDown = async () => {
-    const amtOfRolls = Math.floor(Math.random() * 21);
-    const result = await _rollDown(amtOfRolls, currentNum);
-    result && setCurrentNum(result);
-    setRouletteNum(null);
-  };
-
-  const _rollDown = async (amtOfRolls: number, rolledNum: number) => {
+  const rollDown = () => {
     let curRoll = 1;
+    const amtOfRolls = Math.floor(Math.random() * 21);
+    const maxTime = (amtOfRolls * 100) + 100
+    const finalNum = Math.floor(Math.random() * currentNum) + 1
     while (curRoll < amtOfRolls) {
-      amtOfRolls;
-      await rouletteSpin(curRoll, rolledNum);
+      setTimeout(() => {
+        const newRolNum = Math.floor(Math.random() * currentNum) + 1
+        setRouletteNum(newRolNum);
+      }, curRoll * 100),
       curRoll++;
     }
-    return rouletteNum;
-  };
 
-  const rouletteSpin = (curRoll: number, rolledNum: number): Promise<void> => {
-    return new Promise((resolve) =>
-      setTimeout(() => {
-        setRouletteNum(Math.floor(Math.random() * rolledNum) + 1);
-        resolve();
-      }, curRoll * 100),
-    );
-  };
+    setTimeout(() => {
+      setCurrentNum(finalNum)
+      setRouletteNum(null)
+    }, maxTime)
+ };
 
   const reset = () => {
     setCurrentNum(1000);
@@ -41,7 +32,6 @@ export default function Home() {
   };
   useEffect(() => {
     if (currentNum === 1) setStatus("Defeated");
-    console.log(currentNum);
   }, [currentNum]);
 
   return (
@@ -55,7 +45,7 @@ export default function Home() {
         <div className="text-lg text-slate-500">Deathroll</div>
         <div>{rouletteNum ?? currentNum}</div>
         {status && <div className="m-3 text-red-500">{status}</div>}
-        <button onClick={rollDown} disabled={currentNum === 1}>
+        <button onClick={rollDown} disabled={currentNum === 1 || rouletteNum !== null}>
           Roll
         </button>
         {currentNum === 1 && <button onClick={reset}>Play Again</button>}
